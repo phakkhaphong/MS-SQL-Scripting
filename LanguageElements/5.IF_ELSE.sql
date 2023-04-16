88e0464f1cc0040ef5c6505e38444b5a3a2ef528
phakkhaphong <phakkhaphong@msn.com>
2023-04-16 22:42:49 +0700

    Update 5.IF_ELSE.sql

--- LanguageElements/5.IF_ELSE.sql
+++ LanguageElements/5.IF_ELSE.sql
@@ -6,7 +6,7 @@ IF EXISTS
 	WHERE TotalDue>3900 AND PurchaseOrderNumber IS NULL
 	)
 	BEGIN
-		DECLARE @cnt int =(	SELECT COUNT(*) FROM Sales.SalesOrderHeader WHERE TotalDue>3900 AND PurchaseOrderNumber IS NULL)
+		DECLARE @cnt int =(SELECT COUNT(*) FROM Sales.SalesOrderHeader WHERE TotalDue>3900 AND PurchaseOrderNumber IS NULL)
 		DECLARE @msg varchar(200)=CONCAT(CONVERT(varchar,@cnt),' ','Sales Orders is not valid.')
 		
 		RAISERROR(@msg,16,1)
