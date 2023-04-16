9c7e38cb8c09ed4c86161ad16f3fadb78c318461
phakkhaphong <phakkhaphong@msn.com>
2023-04-16 22:41:58 +0700

    Create 5.IF_ELSE.sql

---
+++ LanguageElements/5.IF_ELSE.sql
@@ -0,0 +1,16 @@
+﻿--IF-ELSE Flow Control
+
+IF EXISTS
+	(
+	SELECT 1 FROM Sales.SalesOrderHeader
+	WHERE TotalDue>3900 AND PurchaseOrderNumber IS NULL
+	)
+	BEGIN
+		DECLARE @cnt int =(	SELECT COUNT(*) FROM Sales.SalesOrderHeader WHERE TotalDue>3900 AND PurchaseOrderNumber IS NULL)
+		DECLARE @msg varchar(200)=CONCAT(CONVERT(varchar,@cnt),' ','Sales Orders is not valid.')
+		
+		RAISERROR(@msg,16,1)
+	END
+
+ELSE RAISERROR('All sales orders are valid.',10,1)
+
