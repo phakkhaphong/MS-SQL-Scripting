0908151711d0c3724cf5f14b266eb1daf97f0b36
phakkhaphong <phakkhaphong@msn.com>
2023-04-17 20:27:47 +0700

    Update 2 Scripts

--- TransactionManagement/04.TransactionInTryCatchBlock.sql
+++ TransactionManagement/04.TransactionInTryCatchBlock.sql
@@ -69,35 +69,40 @@ CREATE TABLE Sales.MiniOrderDetails
 
 */
 
---Test 03
+--Test 04
 
 USE AdventureWorks
 GO
 
 SET XACT_ABORT ON
-BEGIN TRANSACTION
-	INSERT INTO Sales.MiniCustomers 
-	(
-		CompanyName,ContactName,ContactTitle,[Address]
-	,	City,Region,PostalCode,Country,Phone
-	)
-	VALUES
-	(
-		'Data Meccanica Co.,Ltd.','Phakkhaphong K.','Owner'
-	,	'109/6 M9','Paris',NULL,10058,'France','(66) 789-0123'
-	)
-	INSERT INTO Sales.MiniOrders (CustID,OrderDate,Freight)
-	VALUES (@@IDENTITY,GETDATE(),32.38)
-	
-	INSERT INTO Sales.MiniOrderDetails 
-	(OrderID,ProductID,UnitPrice,Quantity,Discount)
-	VALUES
-	(@@IDENTITY,854,27,30,0.2),(@@IDENTITY,859,97,2,0),(@@IDENTITY,860,19,3,0);
-	
-	UPDATE Production.MiniProducts SET Quantity=Quantity-30 WHERE productid=854;
-	UPDATE Production.MiniProducts SET Quantity=Quantity-2 WHERE productid=859;
-	UPDATE Production.MiniProducts SET Quantity=Quantity-3 WHERE productid=860;
-COMMIT TRANSACTION
+BEGIN TRY
+	BEGIN TRANSACTION
+		INSERT INTO Sales.MiniCustomers 
+		(
+			CompanyName,ContactName,ContactTitle,[Address]
+		,	City,Region,PostalCode,Country,Phone
+		)
+		VALUES
+		(
+			'Data Meccanica Co.,Ltd.','Phakkhaphong K.','Owner'
+		,	'109/6 M9','Paris',NULL,10058,'France','(66) 789-0123'
+		)
+		INSERT INTO Sales.MiniOrders (CustID,OrderDate,Freight)
+		VALUES (@@IDENTITY,GETDATE(),32.38)
+
+		INSERT INTO Sales.MiniOrderDetails 
+		(OrderID,ProductID,UnitPrice,Quantity,Discount)
+		VALUES
+		(@@IDENTITY,854,27,30,0.2),(@@IDENTITY,859,97,2,0),(@@IDENTITY,860,19,3,0);
+
+		UPDATE Production.MiniProducts SET Quantity=Quantity-30 WHERE productid=854;
+		UPDATE Production.MiniProducts SET Quantity=Quantity-2 WHERE productid=859;
+		UPDATE Production.MiniProducts SET Quantity=Quantity-3 WHERE productid=860;
+	COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+	IF (XACT_STATE())=-1 ROLLBACK TRANSACTION
+END CATCH 
 SET XACT_ABORT OFF
 
 GO
