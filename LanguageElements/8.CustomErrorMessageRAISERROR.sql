ab605a80c52fcc80980099812f9879fe0aa251a7
phakkhaphong <phakkhaphong@msn.com>
2023-04-17 11:55:46 +0700

    Create 8.CustomErrorMessageRAISERROR.sql

---
+++ LanguageElements/8.CustomErrorMessageRAISERROR.sql
@@ -0,0 +1,29 @@
+﻿USE master;  
+GO  
+  
+EXEC sp_addmessage
+	@msgnum = 55055 
+,	@severity = 16
+,	@msgtext = 
+		'
+		This is a test message with one numeric  
+        parameter (%d) and one string parameter (%s)   
+        '
+,	@lang = 'us_english'; 
+GO
+
+SELECT * FROM sys.messages WHERE message_id=55055;
+GO
+
+RAISERROR(55055,16,111,8999,'AKE!!!');
+GO
+
+RAISERROR
+	(
+	'
+	This is a test message with one numeric  
+    parameter (%d) and one string parameter (%s)   
+    '
+	,16,111,8999,'AKE!!!'
+	);
+GO
