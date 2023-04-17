a1700dc9dd69680b10bb982821df24d907d23f6e
phakkhaphong <phakkhaphong@msn.com>
2023-04-17 14:13:08 +0700

    Add files via upload

---
+++ LanguageElements/04.Predicate.sql
@@ -0,0 +1,32 @@
+﻿--Predicate
+USE AdventureWorks;
+GO
+
+--กรองข้อมูล (ในประโยค WHERE และ/หรือ HAVING)
+
+SELECT * FROM Person.Person 
+WHERE MiddleName=NULL;
+
+
+SELECT * FROM Person.Person 
+WHERE MiddleName IS NOT NULL;
+
+SELECT * FROM Person.Person 
+WHERE PersonType='SP';
+
+SELECT PersonType,COUNT(*) as cnt FROM Person.Person 
+GROUP BY PersonType
+HAVING COUNT(*)>5000;
+
+--เทียบกับคิวรี่ย่อย
+
+SELECT * FROM Sales.SalesOrderHeader as O
+WHERE O.CustomerID IN
+	(
+		SELECT TOP 1 WITH TIES CustomerID FROM Sales.SalesOrderHeader 
+		GROUP BY CustomerID
+		ORDER BY COUNT(*) DESC
+	)
+ORDER BY CustomerID,SalesOrderID;
+
+
