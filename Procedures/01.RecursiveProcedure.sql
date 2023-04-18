30ea83ab5771e9d5b444c607ed246efbe63e396c
phakkhaphong <phakkhaphong@msn.com>
2023-04-18 07:39:25 +0700

    Upload 2 Files

---
+++ Procedures/01.RecursiveProcedure.sql
@@ -0,0 +1,15 @@
+﻿--Create Recursive Procedure
+CREATE PROCEDURE dbo.udpTestRecursive
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	PRINT 'Nest Level ' + CAST(@@NESTLEVEL as varchar(3))
+	IF @@NESTLEVEL=20 RETURN
+
+	EXEC dbo.udpTestRecursive
+
+END;
+
+--Test Recursive Procedure
+EXEC dbo.udpTestRecursive;
