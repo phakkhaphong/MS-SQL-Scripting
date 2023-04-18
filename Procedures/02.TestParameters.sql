7fb1c85ffc0463f48220e1aa6b353d2c2398ee41
phakkhaphong <phakkhaphong@msn.com>
2023-04-18 08:25:54 +0700

    Create 02.TestParameters.sql

---
+++ Procedures/02.TestParameters.sql
@@ -0,0 +1,31 @@
+﻿--Create Test Parameter Procedure
+CREATE OR ALTER PROCEDURE dbo.udpTestParams
+	@param1 decimal(18,2)
+,	@param2 decimal(18,2) OUTPUT
+AS
+BEGIN
+	IF @param2=0 RETURN 1;
+
+	SET @param2=@param1/@param2;
+
+	RETURN 2;
+END
+GO
+
+--Execute 1
+DECLARE @returnInt int;
+DECLARE @result decimal(18,2)=30;
+
+EXEC @returnInt=dbo.udpTestParams @param1=10,@param2=@result OUTPUT;
+
+SELECT @returnInt as [Return Integer], @result as Result;
+GO
+
+--Execute 2
+DECLARE @returnInt int;
+DECLARE @result decimal(18,2)=0;
+
+EXEC @returnInt=dbo.udpTestParams @param1=10,@param2=@result OUTPUT;
+
+SELECT @returnInt as [Return Integer], @result as Result;
+GO
