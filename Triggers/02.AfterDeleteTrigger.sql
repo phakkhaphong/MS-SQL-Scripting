7993114d5ba49c711ed7dc8a1893d07b7b7b35b9
phakkhaphong <phakkhaphong@msn.com>
2023-04-19 10:06:17 +0700

    Create 02.AfterDeleteTrigger.sql

---
+++ Triggers/02.AfterDeleteTrigger.sql
@@ -0,0 +1,23 @@
+﻿--Create After Delete Trigger
+USE AdventureWorks;
+GO
+
+CREATE TRIGGER Sales.tg_SalesTerritoryHistory_Delete
+ON Sales.SalesTerritoryHistory AFTER DELETE AS 
+BEGIN
+  IF EXISTS(SELECT 1
+            FROM deleted AS d
+            WHERE d.EndDate IS NULL) 
+	BEGIN
+		PRINT 'Current Sales Territory History rows cannot be deleted';
+		ROLLBACK;
+	END;
+END;
+
+--Test After Delete Trigger
+
+DELETE Sales.SalesTerritoryHistory WHERE BusinessEntityID=283
+
+--DROP Trigger
+
+DROP TRIGGER IF EXISTS Sales.tg_SalesTerritoryHistory_Delete;
