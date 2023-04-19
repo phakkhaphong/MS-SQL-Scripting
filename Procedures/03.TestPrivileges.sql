9f55f03c39bfcb41b3c1b908eae7f22424e2fcb3
phakkhaphong <phakkhaphong@msn.com>
2023-04-19 09:49:35 +0700

    Create Test Privileges procedures

---
+++ Procedures/03.TestPrivileges.sql
@@ -0,0 +1,17 @@
+﻿USE AdventureWorks;
+GO
+
+--Create Database user without login and not grant any permission
+CREATE USER NoPrivileges WITHOUT LOGIN;
+GO
+
+--Create Test Privileges Procedure
+CREATE OR ALTER PROCEDURE dbo.udpTestPrivileges
+AS
+BEGIN
+	EXECUTE AS USER='NoPrivileges';
+	BACKUP DATABASE AdventureWorks TO DISK='D:\Backups\adv.bak';
+END
+
+--Test
+EXEC dbo.udpTestPrivileges;
