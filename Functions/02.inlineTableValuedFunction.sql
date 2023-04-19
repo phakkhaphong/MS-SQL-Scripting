272af91923e447ae3a107e78bb60e26b034cc591
phakkhaphong <phakkhaphong@msn.com>
2023-04-19 10:42:07 +0700

    Create 02.inlineTableValuedFunction.sql

---
+++ Functions/02.inlineTableValuedFunction.sql
@@ -0,0 +1,30 @@
+﻿--Create Inline table-valued function Function
+USE AdventureWorks;
+GO
+
+CREATE OR ALTER FUNCTION Production.udfGetProductByCatID(@catid int,@rowno int)RETURNS TABLE
+AS
+	RETURN
+		SELECT TOP(@rowno) 
+			P.ProductID
+		,	P.[Name]
+		,	P.ProductNumber
+		,	P.Color
+		,	P.StandardCost
+		,	P.ListPrice
+		,	P.SellStartDate
+		,	P.ModifiedDate
+		FROM Production.Product as P
+		INNER JOIN Production.ProductSubcategory as SC ON P.ProductSubcategoryID=SC.ProductSubcategoryID
+		WHERE SC.ProductCategoryID=@catid
+		ORDER BY P.ListPrice DESC
+GO
+
+--Test Inline table-valued function Function
+SELECT 
+*
+FROM Production.udfGetProductByCatID(4,3)
+GO
+
+--DROP Objects
+DROP FUNCTION IF EXISTS Production.udfGetProductByCatID;
