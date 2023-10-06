41444f63c492cb655837248e612fb3c36b0b6edb
phakkhaphong <phakkhaphong@msn.com>
2023-10-06 10:54:43 +0700

    Create 00.ComparePROCandFUNC.sql

---
+++ Procedures/00.ComparePROCandFUNC.sql
@@ -0,0 +1,40 @@
+--ALTER PROC Sales.udpGetLastOrderIDbyCustomerID
+--	@customerID int
+--,	@MaxOrderID int OUTPUT
+--AS
+--SELECT 
+--	@MaxOrderID=MAX(OI.SalesOrderID)
+--FROM Sales.SalesOrderHeader as OI
+--WHERE OI.CustomerID=@customerID
+--GO
+
+DECLARE @varMaxOrderID int
+
+EXEC Sales.udpGetLastOrderIDbyCustomerID @customerID=29898,@MaxOrderID=@varMaxOrderID OUTPUT
+
+SELECT *
+FROM Sales.SalesOrderDetail 
+WHERE SalesOrderID=@varMaxOrderID
+
+GO
+
+--CREATE FUNCTION Sales.udfGetLastOrderIDbyCustomerID(@customerID int)RETURNS int
+--AS
+--BEGIN
+--	DECLARE @result int
+--	SELECT 
+--		@result=MAX(OI.SalesOrderID)
+--	FROM Sales.SalesOrderHeader as OI
+--	WHERE OI.CustomerID=@customerID
+
+--	RETURN @result
+--END
+--GO
+
+SELECT *
+FROM Sales.SalesOrderDetail 
+WHERE SalesOrderID=Sales.udfGetLastOrderIDbyCustomerID(29898)
+
+GO
+	
+	
